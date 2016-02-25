017105d46dda3d369d10ccf13b154ab5b624bde2
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-02-24 21:31:03 -0300

    Some fixes to Linux

--- application/config/config.php
+++ application/config/config.php
@@ -43,7 +43,7 @@ $config['base_url'] = 'http://localhost/open-idea-php/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
