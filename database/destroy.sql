d2670ca631b0474f0dfe2499788e36c56c6f9d74
Rafael Mateus <rafaelmateus@hotmail.com>
2016-03-15 20:51:47 -0300

    Add scrpit to create and destroy database

---
+++ database/destroy.sql
@@ -0,0 +1,21 @@
+DROP TABLE tb_notification;
+
+DROP TABLE tb_like;
+
+DROP TABLE tb_comment;
+
+DROP TABLE tb_idea;
+
+DROP TABLE tb_challenge;
+
+DROP TABLE tb_user;
+
+DROP TABLE tb_incubator;
+
+DROP TABLE tb_user_type;
+
+DROP TABLE tb_job;
+
+DROP TABLE tb_innovation_area;
+
+
