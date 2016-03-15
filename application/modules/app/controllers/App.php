1f8c5c10d106a1ef20dc4efd566ffb8cc64922be
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-15 19:42:46 -0300

    Test deploy heroku

--- application/modules/app/controllers/App.php
+++ application/modules/app/controllers/App.php
@@ -1,4 +1,5 @@
 <?php defined ('BASEPATH') or exit ('No direct script access allowed');
+	// Test deploy heroku
 class App extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
