fa8b2283ee0d366367fa0706518b43d7ee0e2c24
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-16 11:19:05 -0300

    Update register.php

--- application/modules/sessions/views/register.php
+++ application/modules/sessions/views/register.php
@@ -36,7 +36,7 @@
         <div class="row margin">
           <div class="input-field col s12">
             <!-- <i id="cnpj_icon" class="mdi-action-assignment-ind prefix" hidden></i> -->
-            <input id="cnpj_input" name="cnpj" type="text" hidden required>
+            <input id="cnpj_input" name="cnpj" type="text" hidden>
             <label id="cnpj_label" for="cnpj" class="center-align active" hidden><?php echo $this->lang->line('cnpj'); ?></label>
           </div>
         </div>
