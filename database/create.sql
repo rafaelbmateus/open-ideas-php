4f4dbfcdf5219a34ab6e0394ddde28237650d78a
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-15 21:54:47 -0300

    Fix

--- database/create.sql
+++ database/create.sql
@@ -7,23 +7,23 @@ CREATE TABLE tb_innovation_area (
 PRIMARY KEY(innovation_area_id));
 
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
+INSERT INTO tb_innovation_area(area_name, created_at)
 VALUES ('Medicina', now());
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
+INSERT INTO tb_innovation_area(area_name, created_at)
 VALUES ('Nanotecnologia', now());
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
-VALUES ('Tecnologia da InformaÁ„o', now());
+INSERT INTO tb_innovation_area(area_name, created_at)
+VALUES ('Tecnologia da Informa√ß√£o', now());
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
-VALUES ('Energia distribuÌda', now());
+INSERT INTO tb_innovation_area(area_name, created_at)
+VALUES ('Energia distribu√≠da', now());
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
-VALUES ('BiocombustÌveis', now());
+INSERT INTO tb_innovation_area(area_name, created_at)
+VALUES ('Biocombust√≠veis', now());
 
-INSERT INTO tb_innovation_area(area_name, created_at) 
-VALUES ('ComputaÁ„o pervasiva', now());
+INSERT INTO tb_innovation_area(area_name, created_at)
+VALUES ('Computa√ß√£o pervasiva', now());
 
 commit;
 
@@ -81,49 +81,49 @@ PRIMARY KEY(incubator_id));
 
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('CELTA', 'O Centro Empresarial para LaboraÁ„o de Tecnologias AvanÁadas (CELTA) È a incubadora da FundaÁ„o CERTI situada em FlorianÛpolis (SC). Foi criado em 1986, como resposta aos anseios de desenvolvimento da capital catarinense e com o objetivo de viabilizar um promissor setor econÙmico, aproveitando os talentos e o conhecimento gerados pela UFSC. ', 'http://www.celta.org.br/', '048 3239-2222', 'SC', 'FlorianÛpolis', now());
+VALUES ('CELTA', 'O Centro Empresarial para Labora√ß√£o de Tecnologias Avan√ßadas (CELTA) √© a incubadora da Funda√ß√£o CERTI situada em Florian√≥polis (SC). Foi criado em 1986, como resposta aos anseios de desenvolvimento da capital catarinense e com o objetivo de viabilizar um promissor setor econ√¥mico, aproveitando os talentos e o conhecimento gerados pela UFSC. ', 'http://www.celta.org.br/', '048 3239-2222', 'SC', 'Florian√≥polis', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('MIDI TecnolÛgico', 'A incubadora MIDI TecnolÛgico, criada no dia 14 de agosto de 1998, tem o ServiÁo de Apoio ‡s Micro e Pequenas Empresas de Santa Catarina (SEBRAE/SC) como entidade mantenedora e a AssociaÁ„o Catarinense de Empresas de Tecnologia (ACATE) como entidade gestora. Localizada em FlorianÛpolis, abriga empresas incubadas cujos produtos, processos ou serviÁos s„o gerados a partir de resultados de pesquisas cientÌficas aplicadas, nos quais a tecnologia e a inovaÁ„o representam alto valor agregado. O MIDI TecnolÛgico tem como objetivo prestar serviÁos de IncubaÁ„o, para o desenvolvimento de empreendimentos nascentes de base tecnolÛgica, visando a criaÁ„o de empresas inovadoras e sustent·veis.', 'http://miditecnologico.com.br/', '048 2107-2737', 'SC', 'FlorianÛpolis', now());
+VALUES ('MIDI Tecnol√≥gico', 'A incubadora MIDI Tecnol√≥gico, criada no dia 14 de agosto de 1998, tem o Servi√ßo de Apoio √†s Micro e Pequenas Empresas de Santa Catarina (SEBRAE/SC) como entidade mantenedora e a Associa√ß√£o Catarinense de Empresas de Tecnologia (ACATE) como entidade gestora. Localizada em Florian√≥polis, abriga empresas incubadas cujos produtos, processos ou servi√ßos s√£o gerados a partir de resultados de pesquisas cient√≠ficas aplicadas, nos quais a tecnologia e a inova√ß√£o representam alto valor agregado. O MIDI Tecnol√≥gico tem como objetivo prestar servi√ßos de Incuba√ß√£o, para o desenvolvimento de empreendimentos nascentes de base tecnol√≥gica, visando a cria√ß√£o de empresas inovadoras e sustent√°veis.', 'http://miditecnologico.com.br/', '048 2107-2737', 'SC', 'Florian√≥polis', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('CRIE', 'A incubadora do Centro Regional de InovaÁ„o e Empreendedorismo (Crie) atua desde 2005 para viabilizar projetos de micro e pequenas empresas. … uma organizaÁ„o civil sem fins lucrativos, com personalidade jurÌdica de direito privado, com autonomia administrativa e financeira. A partir de suporte tÈcnico, gerencial e infraestrutura, a incubadora Crie contribui para o desenvolvimento regional sustent·vel oferecendo ‡s empresas nascentes, por baixo custo, espaÁo e serviÁos b·sicos, alÈm de um conjunto de orientaÁıes tÈcnicas e gerenciais. O espaÁo promove ainda a sinergia entre empresas incubadas com instituiÁıes de ensino e pesquisa, empresas, Ûrg„os governamentais, associaÁıes de classe, agentes e mercado consumidor.', 'http://www.unisul.br/wps/portal/home/pesquisa-e-inovacao/agetec/incubadora-crie', '048 3621-3406', 'SC', 'FlorianÛpolis', now());
+VALUES ('CRIE', 'A incubadora do Centro Regional de Inova√ß√£o e Empreendedorismo (Crie) atua desde 2005 para viabilizar projetos de micro e pequenas empresas. √â uma organiza√ß√£o civil sem fins lucrativos, com personalidade jur√≠dica de direito privado, com autonomia administrativa e financeira. A partir de suporte t√©cnico, gerencial e infraestrutura, a incubadora Crie contribui para o desenvolvimento regional sustent√°vel oferecendo √†s empresas nascentes, por baixo custo, espa√ßo e servi√ßos b√°sicos, al√©m de um conjunto de orienta√ß√µes t√©cnicas e gerenciais. O espa√ßo promove ainda a sinergia entre empresas incubadas com institui√ß√µes de ensino e pesquisa, empresas, √≥rg√£os governamentais, associa√ß√µes de classe, agentes e mercado consumidor.', 'http://www.unisul.br/wps/portal/home/pesquisa-e-inovacao/agetec/incubadora-crie', '048 3621-3406', 'SC', 'Florian√≥polis', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
 VALUES ('LNCC', '', 'http://www.incubadora.lncc.br/', '024 2233-6232', 'RJ', '', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('Santos Dumont', 'A Incubadora Santos Dumont È um espaÁo que oferece suporte gerencial, facilitando a interaÁ„o com o meio empresarial, cientÌfico, tecnolÛgico e financeiro, visando a sua inserÁ„o, crescimento e consolidaÁ„o no mercado.', 'http://www.pti.org.br/incubadorasantosdumont', '045 3576-7073', 'PR', 'Foz do IguaÁu', now());
+VALUES ('Santos Dumont', 'A Incubadora Santos Dumont √© um espa√ßo que oferece suporte gerencial, facilitando a intera√ß√£o com o meio empresarial, cient√≠fico, tecnol√≥gico e financeiro, visando a sua inser√ß√£o, crescimento e consolida√ß√£o no mercado.', 'http://www.pti.org.br/incubadorasantosdumont', '045 3576-7073', 'PR', 'Foz do Igua√ßu', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('ITSM', 'As instituiÁıes de ensino e de pesquisa tecnolÛgica tÍm como obrigaÁ„o primeira acompanhar o processo de inovaÁ„o e de mudanÁa de paradigmas. A empresa inovadora, forte dependente de novas tecnologias e novos conhecimentos para o sucesso de seus negÛcios, de certa forma, tambÈm È dependente dos esforÁos despendidos pelo corpo de pesquisadores das instituiÁıes, para ver suas aspiraÁıes contempladas.', 'http://coral.ufsm.br/itsm/', '055 3220-8836', 'RS', 'Santa Maria', now());
+VALUES ('ITSM', 'As institui√ß√µes de ensino e de pesquisa tecnol√≥gica t√™m como obriga√ß√£o primeira acompanhar o processo de inova√ß√£o e de mudan√ßa de paradigmas. A empresa inovadora, forte dependente de novas tecnologias e novos conhecimentos para o sucesso de seus neg√≥cios, de certa forma, tamb√©m √© dependente dos esfor√ßos despendidos pelo corpo de pesquisadores das institui√ß√µes, para ver suas aspira√ß√µes contempladas.', 'http://coral.ufsm.br/itsm/', '055 3220-8836', 'RS', 'Santa Maria', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('Instituto GENE', 'O Instituto Gene È uma associaÁ„o civil sem fins lucrativos que tem como objetivo promover o desenvolvimento multissetorial por meio do estÌmulo e suporte ‡ inovaÁ„o e ao empreendedorismo. Atualmente, vem atuando diretamente no apoio a geraÁ„o de empresas inovadoras, por meio da Incubadora de Empresas e, subsequentemente, captaÁ„o de recursos por meio de projetos.', 'http://www.institutogene.org.br/', '047 3321-7870', 'SC', 'Blumenau', now());
+VALUES ('Instituto GENE', 'O Instituto Gene √© uma associa√ß√£o civil sem fins lucrativos que tem como objetivo promover o desenvolvimento multissetorial por meio do est√≠mulo e suporte √† inova√ß√£o e ao empreendedorismo. Atualmente, vem atuando diretamente no apoio a gera√ß√£o de empresas inovadoras, por meio da Incubadora de Empresas e, subsequentemente, capta√ß√£o de recursos por meio de projetos.', 'http://www.institutogene.org.br/', '047 3321-7870', 'SC', 'Blumenau', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('RAIAR', 'A RAIAR ñ Incubadora de Empresas da PUCRS tem como propÛsito estimular e operacionalizar a vis„o empreendedora da comunidade PUCRS. Por meio do Programa de IncubaÁ„o e do Startup Garagem, a Raiar apoia projetos de negÛcio, dando suporte em assessorias e infraestrutura, transformando-os em empreendimentos competitivos e prontos para atuarem no mercado. Saiba mais sobre a Raiar', 'http://www3.pucrs.br/portal/page/portal/inovapucrs/Capa/raiar', '051 3320-3673', 'RS', 'Porto Alegre', now());
+VALUES ('RAIAR', 'A RAIAR ‚Äì Incubadora de Empresas da PUCRS tem como prop√≥sito estimular e operacionalizar a vis√£o empreendedora da comunidade PUCRS. Por meio do Programa de Incuba√ß√£o e do Startup Garagem, a Raiar apoia projetos de neg√≥cio, dando suporte em assessorias e infraestrutura, transformando-os em empreendimentos competitivos e prontos para atuarem no mercado. Saiba mais sobre a Raiar', 'http://www3.pucrs.br/portal/page/portal/inovapucrs/Capa/raiar', '051 3320-3673', 'RS', 'Porto Alegre', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('Softville', '… uma FundaÁ„o de car·ter tÈcnico cientÌfico, com personalidade jurÌdica de direito privado, sem fins lucrativos. Miss„o: Proporcionar o apoio necess·rio para o desenvolvimento de empresas de Base TecnolÛgica, contribuindo para o seu amadurecimento e consequente sucesso rumo ao mercado globalizado.', 'http://www.softville.org.br/', '047 3431-7300', 'SC', 'Joinville', now());
+VALUES ('Softville', '√â uma Funda√ß√£o de car√°ter t√©cnico cient√≠fico, com personalidade jur√≠dica de direito privado, sem fins lucrativos. Miss√£o: Proporcionar o apoio necess√°rio para o desenvolvimento de empresas de Base Tecnol√≥gica, contribuindo para o seu amadurecimento e consequente sucesso rumo ao mercado globalizado.', 'http://www.softville.org.br/', '047 3431-7300', 'SC', 'Joinville', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('EDUCERE', 'A FundaÁ„o Educere È um Centro de Pesquisas e Desenvolvimento na ·rea de biotecnologia, cujo foco principal È a incubaÁ„o de empresas a partir de um projeto social inovador que atua na formaÁ„o de jovens com potencial empreendedor.', 'http://www.educere.org.br/', '', 'PR', 'Campo Mour„o', now());
+VALUES ('EDUCERE', 'A Funda√ß√£o Educere √© um Centro de Pesquisas e Desenvolvimento na √°rea de biotecnologia, cujo foco principal √© a incuba√ß√£o de empresas a partir de um projeto social inovador que atua na forma√ß√£o de jovens com potencial empreendedor.', 'http://www.educere.org.br/', '', 'PR', 'Campo Mour√£o', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('CIETEC', 'Cietec È uma associaÁ„o civil sem fins lucrativos de direito privado estabelecida com a miss„o de promover o Empreendedorismo Inovador, incentivando a transformaÁ„o do conhecimento em produtos e serviÁos de valor agregado para o mercado.', 'http://www.cietec.org.br/', '011 3039-8300', 'SP', 'Cidade Universit·ria de S„o Paulo', now());
+VALUES ('CIETEC', 'Cietec √© uma associa√ß√£o civil sem fins lucrativos de direito privado estabelecida com a miss√£o de promover o Empreendedorismo Inovador, incentivando a transforma√ß√£o do conhecimento em produtos e servi√ßos de valor agregado para o mercado.', 'http://www.cietec.org.br/', '011 3039-8300', 'SP', 'Cidade Universit√°ria de S√£o Paulo', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('Mackenzie', 'A incubadora de empresas do Mackenzie È um ambiente de fomento e apoio ao desenvolvimento de novas empresas inovadoras - de prestaÁ„o de serviÁos, de base tecnolÛgica ou de manufaturas leves - oferecendo suporte tÈcnico, gerencial e formaÁ„o complementar ao empreendedor.', 'http://www.mackenzie.br/', '011 2114-8074', 'SP', 'Vila Buarque', now());
+VALUES ('Mackenzie', 'A incubadora de empresas do Mackenzie √© um ambiente de fomento e apoio ao desenvolvimento de novas empresas inovadoras - de presta√ß√£o de servi√ßos, de base tecnol√≥gica ou de manufaturas leves - oferecendo suporte t√©cnico, gerencial e forma√ß√£o complementar ao empreendedor.', 'http://www.mackenzie.br/', '011 2114-8074', 'SP', 'Vila Buarque', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('CIATEC', 'A CIATEC participa ativamente no planejamento e na execuÁ„o da polÌtica de ciÍncia e tecnologia da administraÁ„o p˙blica das diversas esferas governamentais especialmente a municipal. AtravÈs de sua Incubadora de Empresas de Base TecnolÛgica, auxilia no surgimento e na evoluÁ„o de empreendimentos geradores de novas tecnologias, ajudando a consolidar Campinas como uma das referÍncias no mundo.', 'http://www.ciatec.org.br/', '019 3756-5433', 'SP', 'Campinas', now());
+VALUES ('CIATEC', 'A CIATEC participa ativamente no planejamento e na execu√ß√£o da pol√≠tica de ci√™ncia e tecnologia da administra√ß√£o p√∫blica das diversas esferas governamentais especialmente a municipal. Atrav√©s de sua Incubadora de Empresas de Base Tecnol√≥gica, auxilia no surgimento e na evolu√ß√£o de empreendimentos geradores de novas tecnologias, ajudando a consolidar Campinas como uma das refer√™ncias no mundo.', 'http://www.ciatec.org.br/', '019 3756-5433', 'SP', 'Campinas', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('INCAMP', 'A Incubadora de Empresas de Base TecnolÛgica da Unicamp (INCAMP), fundada em 2001 e administrada pela AgÍncia de InovaÁ„o Inova Unicamp, È o ambiente ideal para que projetos tecnolÛgicos virem verdadeiros negÛcios. AlÈm do espaÁo fÌsico, proporciona todo o suporte necess·rio para que essas empresas cheguem ao mercado e se tornem empreendimentos de sucesso, por meio de reuniıes e mentorias com especialistas e profissionais do ecossistema de empreendedorismo e inovaÁ„o, eventos, palestras e encontros com investidores.', 'http://www.inova.unicamp.br/', '019 3521-5203', 'SP', 'Campinas', now());
+VALUES ('INCAMP', 'A Incubadora de Empresas de Base Tecnol√≥gica da Unicamp (INCAMP), fundada em 2001 e administrada pela Ag√™ncia de Inova√ß√£o Inova Unicamp, √© o ambiente ideal para que projetos tecnol√≥gicos virem verdadeiros neg√≥cios. Al√©m do espa√ßo f√≠sico, proporciona todo o suporte necess√°rio para que essas empresas cheguem ao mercado e se tornem empreendimentos de sucesso, por meio de reuni√µes e mentorias com especialistas e profissionais do ecossistema de empreendedorismo e inova√ß√£o, eventos, palestras e encontros com investidores.', 'http://www.inova.unicamp.br/', '019 3521-5203', 'SP', 'Campinas', now());
 
 INSERT INTO tb_incubator (incubator_name, incubator_description, incubator_url, incubator_phone, incubator_state, incubator_city, created_at)
-VALUES ('CIDE', 'O Centro de IncubaÁ„o e Desenvolvimento Empresarial ñ CIDE, tem a miss„o de estruturar a criaÁ„o e o desenvolvimento de empresas inovadoras de base tecnolÛgica com Ínfase nos setores de biotecnologia, tecnologia da informaÁ„o e eletrÙnica, atravÈs de aÁıes que contribuam para incentivar o empreendedor e o desenvolvimento socioeconÙmico do Estado. Para atingir sua finalidades o CIDE utiliza o sistema de incubaÁ„o de empresas, fazendo ìponteî entre o mercado e o desenvolvimento tecnolÛgico, respeitando as caracterÌsticas prÛprias da regi„o.', 'http://cide.org.br/', '', 'AM', 'Manaus', now());
+VALUES ('CIDE', 'O Centro de Incuba√ß√£o e Desenvolvimento Empresarial ‚Äì CIDE, tem a miss√£o de estruturar a cria√ß√£o e o desenvolvimento de empresas inovadoras de base tecnol√≥gica com √™nfase nos setores de biotecnologia, tecnologia da informa√ß√£o e eletr√¥nica, atrav√©s de a√ß√µes que contribuam para incentivar o empreendedor e o desenvolvimento socioecon√¥mico do Estado. Para atingir sua finalidades o CIDE utiliza o sistema de incuba√ß√£o de empresas, fazendo ‚Äúponte‚Äù entre o mercado e o desenvolvimento tecnol√≥gico, respeitando as caracter√≠sticas pr√≥prias da regi√£o.', 'http://cide.org.br/', '', 'AM', 'Manaus', now());
 
 commit;
 
@@ -285,6 +285,3 @@ CREATE INDEX tb_notification_FKIndex1 ON tb_notification (user_id);
 
 
 CREATE INDEX IFK_Rel_08 ON tb_notification (user_id);
-
-
-
