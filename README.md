1cbb7d01ec81ee508307ccb08ab94fef94aa4ec0
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-19 08:49:48 -0300

    Fix

--- README.md
+++ README.md
@@ -4,44 +4,17 @@
 
 O Open Idea é uma plataforma de inovação aberta onde aproxima os desafios das ideias.<br>
 
-A pricipal "idea" é permitir que empresários publiquem desafios onde pesquisadores, acadêmicos e outras fontes externas colaborem para essa solução.<br>
+A pricipal "idea" é permitir que empresas publiquem desafios e que tenham auxílio de pesquisadores, acadêmicos e outras fontes externas colaborem para essa solução.<br>
+As empresas
 
 Alguns valores da Inovação Aberta são:
 * Colaboração em rede
 * Experiências
 * Disseminação de conhecimento
 * Novas parcerias
-* ...
 
 ## TODOs:
 
-### Cors
-
-#### Ideias
-- [x] Listar idea
-- [x] Mostrar idea
-- [x] Cadastrar idea
-- [x] Alterar idea
-- [x] Excluir idea
-
-#### Desafios
-- [x] Listar desafios
-- [x] Mostar desafio
-- [x] Cadastrar desafios
-- [x] Alterar desafio
-- [x] Exluir desafio
-
-#### Usuário
-- [x] Cadastrar
-- [x] Atualizar
-- [x] Mostrar
-
-#### Incubadoras
-- [x] Listar
-- [x] Cadastrar
-- [x] Atualizar
-
-### Específicos
 - [ ] Validar CNPJ - controller
 - [ ] Máscara CNPJ
 - [ ] Layout botão excluir
@@ -50,31 +23,20 @@ Alguns valores da Inovação Aberta são:
 - [ ] Search bar - Google API
 - [ ] Allow create challenge only corporate profile - validate in controller
 - [ ] Buscar idea por area
-- [x] Traduzir calendário
-- [x] Aumentar tamanho do campo no input description do Desafio para 1200
-- [x] Aumentar tamanho do campo no input title do Desafio para 80
 - [ ] Página de ajuda
 - [ ] Salvar email no cookie quando der lock
-- [ ] Remover icone CNPJ no abrir uma conta
-- [x] Alinhar desafios (index)
-- [x] Listar Ideas e desafios na página do usuário
 - [ ] Criar rotas
 - [ ] Translate app_langue to English
-- [x] Trocar ação floating button
 - [ ] Ação javascript no compartilhar, colocar no ctrl+c.
-- [ ] Fazer uma página para acessar do sistema o compartilhamento da ideia/desafio
-- [x] Atualizar perfil
-- [x] Icone iLab
-- [ ] Icone AGETEC
-- [ ] Icone Unisul
-- [x] Comentar em uma ideia
-- [x] Comentar em um desafio
+- [ ] Página para acesso externo da ideia/desafio
 - [ ] Curtir uma ideia
+- [ ] Curtir um desafio
 - [ ] Curtir um desafio
 
 ### Outros para quando tiver tempo...
 - [ ] Seguir usuários
 - [ ] Pontuar o usuário por cada ação. (Gamification)
+- [ ] Adicionar dashboard de ideas
 
 
 External sources help you complete the challenge;<br>
--- application/modules/challenges/views/index.php
+++ application/modules/challenges/views/index.php
@@ -3,34 +3,36 @@
 <?php if (!empty($list)) { ?>
 	<div class="row">
 	<?php foreach($list as $item) { ?>
+		<a href="<?php echo base_url() . $module . '/show/' . $item->challenge_id; ?>">
 		<div class="blog col s4">
 			<br>
-			<div class="card hoverable medium">
-				<div class="card-image waves-effect waves-block waves-light">
-					<!-- <a href="<?php echo base_url() . $module . '/show/' . $item->challenge_id; ?>"><img src="<?php echo base_url(); ?>assets/plugins/materialize/images/img4.jpg" alt="blog-img"></a> -->
-					<img class="activator" src="<?php echo base_url() . 'assets/images/areas/' . $item->area_id . '.jpg'; ?>" alt="blog-img" height="300">
-				</div>
-				<ul class="card-action-buttons">
-					<li><a class="btn-floating waves-effect waves-light light-blue" onclick="like();"><i class="fa fa-thumbs-up"></i></a></li>
-					<li><a class="btn-floating waves-effect waves-light light-blue" onclick="share();"><i class="mdi-social-share"></i></a></li>
-					<li><a class="btn-floating waves-effect waves-light light-blue" href="<?php echo base_url() . $module . '/show/' . $item->challenge_id; ?>"><i class="mdi-action-info activator"></i></a></li>
-				</ul>
-				<div class="card-content">
-					<p class="row">
-						<!-- <?php print_r($list_areas); ?> -->
-						<span class="left"><a href=""><?php echo $list_areas[$item->area_id-1]->area_name; ?></a></span> <!-- TODO: Fix this, $area->name -->
-						<span class="right"><?php echo date('d/m/Y H:i:s', strtotime($item->created_at)); ?></span>
-					</p>
-					<h4 class="card-title grey-text text-darken-4"><a href="#" class="grey-text text-darken-4"><?php echo $item->challenge_title; ?></a></h4>
-					<div class="row">
-						<div class="col s2">
-							<a href="<?php echo base_url() . 'users' . '/show/' . $item->user_id; ?>"><img src="<?php echo "http://www.gravatar.com/avatar/" . md5(strtolower(trim($list_users[$item->user_id-1]->user_email))); ?>" alt="" class="circle responsive-img valign profile-image"></a>
+				<div class="card hoverable medium">
+					<div class="card-image waves-light">
+						<!-- <a href="<?php echo base_url() . $module . '/show/' . $item->challenge_id; ?>"><img src="<?php echo base_url(); ?>assets/plugins/materialize/images/img4.jpg" alt="blog-img"></a> -->
+						<img class="activator" src="<?php echo base_url() . 'assets/images/areas/' . $item->area_id . '.jpg'; ?>" alt="blog-img" height="300">
+					</div>
+					<ul class="card-action-buttons">
+						<li><a class="btn-floating waves-effect waves-light light-blue" onclick="like();"><i class="fa fa-thumbs-up"></i></a></li>
+						<li><a class="btn-floating waves-effect waves-light light-blue" onclick="share();"><i class="mdi-social-share"></i></a></li>
+						<!-- <li><a class="btn-floating waves-effect waves-light light-blue" href="<?php echo base_url() . $module . '/show/' . $item->challenge_id; ?>"><i class="mdi-action-info activator"></i></a></li> -->
+					</ul>
+					<div class="card-content">
+						<p class="row">
+							<!-- <?php print_r($list_areas); ?> -->
+							<span class="left"><a href=""><?php echo $list_areas[$item->area_id-1]->area_name; ?></a></span> <!-- TODO: Fix this, $area->name -->
+							<span class="right"><?php echo date('d/m/Y H:i:s', strtotime($item->created_at)); ?></span>
+						</p>
+						<h4 class="card-title grey-text text-darken-4"><a href="#" class="grey-text text-darken-4"><?php echo $item->challenge_title; ?></a></h4>
+						<div class="row">
+							<div class="col s2">
+								<a href="<?php echo base_url() . 'users' . '/show/' . $item->user_id; ?>"><img src="<?php echo "http://www.gravatar.com/avatar/" . md5(strtolower(trim($list_users[$item->user_id-1]->user_email))); ?>" alt="" class="circle responsive-img valign profile-image"></a>
+							</div>
+							<div class="col s9"><a href="<?php echo base_url() . 'users' . '/show/' . $item->user_id; ?>"><?php echo $list_users[$item->user_id-1]->user_name; ?></a></div>
 						</div>
-						<div class="col s9"><a href="<?php echo base_url() . 'users' . '/show/' . $item->user_id; ?>"><?php echo $list_users[$item->user_id-1]->user_name; ?></a></div>
 					</div>
 				</div>
-			</div>
 		</div>
+	</a>
 	<?php } ?>
 	</div>
 <?php }else{ ?>
