83924a59f7ef3560df5514974c0874fb81e248f1
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-23 07:30:15 -0300

    Change button color

--- application/modules/ideas/views/index.php
+++ application/modules/ideas/views/index.php
@@ -16,7 +16,7 @@
         </div>
         <div class="card-content">
           <?php if ($item->user_id == $this->session->userdata('user_id')){ ?>
-            <a class="btn-floating waves-effect waves-light light-blue right" href="<?php echo base_url () . $module . '/edit/' . $item->idea_id; ?>"><i class="fa fa-pencil"></i></a>
+            <a class="btn-floating waves-effect waves-light cyan right" href="<?php echo base_url () . $module . '/edit/' . $item->idea_id; ?>"><i class="fa fa-pencil"></i></a>
           <?php } ?>
           <a href="<?php echo base_url() . 'users' . '/show/' . $item->user_id; ?>"><img width="50" src="<?php echo "http://www.gravatar.com/avatar/" . md5(strtolower(trim($list_users[$item->user_id-1]->user_email))); ?>" alt="" class="circle responsive-img valign profile-image"> <?php echo $list_users[$item->user_id-1]->user_name; ?></a>
           <br><br>
@@ -36,8 +36,8 @@
   							<a id="like<?php echo $item->idea_id; ?>" value="0" class="btn-floating waves-light" onclick="like_manager(<?php echo $item->idea_id; ?>);"><i class="fa fa-thumbs-up"></i></a>
   						<?php } ?>
               <!-- <a class="btn-floating waves-effect waves-light light-blue" onclick="like();"><i class="fa fa-thumbs-up"></i></a> -->
-              <a class="btn-floating waves-effect waves-light light-blue" onclick="share();"><i class="mdi-social-share"></i></a>
-              <a class="btn-floating waves-effect waves-light light-blue" href="<?php echo base_url() . $module . '/show/' . $item->idea_id; ?>"><i class="mdi-action-info activator"></i></a>
+              <a class="btn-floating waves-effect waves-light cyan" onclick="share();"><i class="mdi-social-share"></i></a>
+              <a class="btn-floating waves-effect waves-light cyan" href="<?php echo base_url() . $module . '/show/' . $item->idea_id; ?>"><i class="mdi-action-info activator"></i></a>
             </div>
           </div>
         </div>
