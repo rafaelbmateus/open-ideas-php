679abf6f105a22e2362fc909693a6d24087ce1c6
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-03-22 22:13:44 -0300

    Lenght button confirm challenge

--- application/modules/challenges/views/_form.php
+++ application/modules/challenges/views/_form.php
@@ -61,12 +61,9 @@
 
 <br><br>
 
-<!-- buttons confirm/cancel -->
+<!-- buttons confirm -->
 <div class="row">
-	<div class="input-field col s5">
-		<!-- <a class="btn cyan waves-effect waves-light" href="<?php echo base_url() . $module;?>"><?=$this->lang->line('cancel');?><i class="fa fa-times left"></i></a> -->
-	</div>
 	<div class="input-field col s7">
-		<button class="btn cyan waves-effect waves-light right" type="submit"><?php echo $this->lang->line('save');?><i class="fa fa-check right"></i></button>
+		<button class="btn cyan waves-effect waves-light col s7" type="submit"><?php echo $this->lang->line('save');?> <i class="fa fa-check right"></i></button>
 	</div>
 </div>
